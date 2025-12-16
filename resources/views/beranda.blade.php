368f40993c6204ca9a72eda68878b4f1af4e8135
primaseptiiian <primaseptian@webmail.umm.ac.id>
2025-12-16 17:22:18 +0700

    Update beranda.blade.php

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -78,7 +78,7 @@
             </div>
         </div>
 
-        <!-- Bottom Navbar -->
+        <!--- Bottom Navbar --->
         <div class="fixed bottom-0 w-full max-w-[420px] bg-[#EBEBEB] border-t border-gray-300 rounded-t-[30px] pb-4 pt-4 px-10 flex justify-between items-center z-50">
             
             <a href="#" class="flex flex-col items-center justify-center text-black">
